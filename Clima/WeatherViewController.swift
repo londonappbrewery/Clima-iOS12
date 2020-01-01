d0b40673da20f698da5d98189b41a251723fe255
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2020-01-01 14:08:12 +0530

    Location manager added

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -18,7 +19,7 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -32,7 +33,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
         
     }
